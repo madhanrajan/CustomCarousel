01b58c871e493b830b5fd62b2af518cf7c72b92f
Madhan <madhanrajan.varadharajan@gmail.com>
2024-08-14 15:12:58 +0800

    Initial Commit

---
+++ CustomCarousel/CustomCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCarouselApp.swift
+//  CustomCarousel
+//
+//  Created by Madhanrajan Varadharajan  on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
